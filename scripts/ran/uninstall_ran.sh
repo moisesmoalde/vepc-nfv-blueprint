5e1dd1c1e97ac155ba2d325b3a6b87dd5c1d8a75
moisesmoalde <moisesmoalde@hotmail.com>
2017-06-28 12:57:04 +0200

    Uninstall openvpn package during uninstall workflow at RAN and SINK nodes

--- scripts/ran/uninstall_ran.sh
+++ scripts/ran/uninstall_ran.sh
@@ -3,6 +3,6 @@
 RAN_DIR=$(ctx instance runtime-properties ran_dir)
 sudo rm -r ${RAN_DIR}
 
-sudo apt-get -y remove iperf3 iperf htop openssl libssl-dev
-sudo apt-get -y autoremove
+sudo apt-get -y remove iperf3 iperf htop openssl libssl-dev openvpn
 sudo add-apt-repository -y -r "ppa:patrickdk/general-lucid"
+sudo apt-get -y autoremove
--- scripts/sink/uninstall_sink.sh
+++ scripts/sink/uninstall_sink.sh
@@ -3,6 +3,6 @@
 SINK_DIR=$(ctx instance runtime-properties sink_dir)
 sudo rm -r ${SINK_DIR}
 
-sudo apt-get -y remove iperf3 iperf htop
-sudo apt-get -y autoremove
+sudo apt-get -y remove iperf3 iperf htop openvpn
 sudo add-apt-repository -y -r "ppa:patrickdk/general-lucid"
+sudo apt-get -y autoremove
