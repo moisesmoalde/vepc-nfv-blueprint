491396246ca639a145cbca2723030e7a0230bcd6
moisesmoalde <moisesmoalde@hotmail.com>
2017-09-05 20:07:47 +0200

    Create first version of load balancing runtime configuration script lb.py

---
+++ scripts/lb/lb.py
@@ -0,0 +1,40 @@
+
+# Import Cloudify's context object.
+# This provides several useful functions as well as allowing to pass
+# contextual information of an application.
+from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
+from cloudify import exceptions
+from cloudify import utils
+
+
+def run(command, errorMessage):
+    runner = utils.LocalCommandRunner(logger=ctx.logger)
+    try:
+        runner.run(command)
+    except exceptions.CommandExecutionException as e:
+        raise exceptions.NonRecoverableError('{0}: {1}'.format(
+                error_message, e))
+
+def addBackend(virtual_ip = None, portList = None, backendAddress = None, add = True):
+	vip = virtual_ip or ctx.target.instance.runtime_properties.get('virtual_ip')
+	ports = portList or ctx.target.node.properties['ports']
+	ip = backendAddress or ctx.source.instance.host_ip
+	for port in str(ports).split(","):
+		run("ipvsadm -{0} -u {1}:{2} -r {3}:{4} -g".format(("a" if add else "d"), vip, port, ip, port),
+			"Error adding a real server to a virtual service" if add else
+			"Error removing a real server from a virtual service")
+
+def removeBackend(vip = None, portList = None, backendAddress = None):
+	addBackend(vip, portList, backendAddress, False)
+
+def main():
+	invocation = inputs['invocation']
+	function = invocation['function']
+	args = invocation.get('args', [])
+	kwargs = invocation.get('kwargs', {})
+	globals()[function](*args, **kwargs)
+
+
+if __name__ == '__main__':
+	main()
