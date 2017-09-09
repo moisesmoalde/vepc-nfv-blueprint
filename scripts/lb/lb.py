356a6a9a221ccffab1a1d82cf29c5b1174631cd5
moisesmoalde <moisesmoalde@hotmail.com>
2017-09-09 14:32:24 +0200

    Print log info while adding/removing a real backend server to a virtual server at LB

--- scripts/lb/lb.py
+++ scripts/lb/lb.py
@@ -7,6 +7,8 @@ from cloudify.state import ctx_parameters as inputs
 from cloudify import exceptions
 from cloudify import utils
 
+ADD_MESSAGE = "Adding a real server to a virtual service"
+RM_MESSAGE = "Removing a real server from a virtual service"
 
 def run(command, errorMessage):
     runner = utils.LocalCommandRunner(logger=ctx.logger)
@@ -20,11 +22,11 @@ def addBackend(virtual_ip = None, portList = None, backendAddress = None, add = 
 	vip = virtual_ip or ctx.target.instance.runtime_properties.get('virtual_ip')
 	ports = portList or ctx.target.node.properties['ports']
 	ip = backendAddress or ctx.source.instance.host_ip
+	ctx.logger.info(ADD_MESSAGE if add else RM_MESSAGE)
 	for port in str(ports).split(","):
 		run("sudo ipvsadm -{0} -u {1}:{2} -r {3}:{4}"
 			.format(("a" if add else "d"), str(vip), port, str(ip), port),
-			"Error adding a real server to a virtual service" if add else
-			"Error removing a real server from a virtual service")
+			"Error -> {0}".format(ADD_MESSAGE if add else RM_MESSAGE))
 
 def removeBackend(vip = None, portList = None, backendAddress = None):
 	addBackend(vip, portList, backendAddress, False)
