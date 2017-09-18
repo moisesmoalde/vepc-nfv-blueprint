ce75fd7bc01416a7f6a715b51ab5ef2d2cd3fc7c
moisesmoalde <moisesmoalde@hotmail.com>
2017-09-18 19:21:04 +0200

    Remove unused KVstore config parameters

--- src/utils.h
+++ src/utils.h
@@ -10,7 +10,7 @@
  * 
  * */
 
-#define UE_BINDING 2   //CHANGE THIS TO CHANGE SYNC MODE IN CONTROL PLANE
+#define UE_BINDING 3   //CHANGE THIS TO CHANGE SYNC MODE IN CONTROL PLANE
 
 #define STATE_LESS 1
 #define SESSION_BASED 2
@@ -87,71 +87,16 @@
 #include <boost/serialization/vector.hpp>
 
 
-#if DSR == 0
-	#include <kvstore/KVStoreHeader.h>  // for leveldb
-	#define PDS "10.129.26.81:8090"
-#endif
-	
-#if DSR == 1
-	#include <kvstore/KVStoreHeader.h>  //for redis
-	#define PDS "10.129.28.141:7001"
-#endif
+#include <kvstore/KVStoreHeader.h>  // for leveldb
+#define PDS "10.129.26.81:8090"
 
-
-#if DSR == 2
-	#include <kvstore/KVStoreHeader.h>   // for ramcloud	
-	#define PDS "tcp:host=10.129.26.81,port=11100";
-	#endif
-
-#if DSR == 3
-	#include <kvstore/KVStoreHeader.h>  //for memcacheDB single server config
-	#define PDS "--SERVER=10.129.28.141:12000"
-#endif
-
-#if DSR == 4
-	#include <kvstore/KVStoreHeader.h>  //for memcacheDB multiple servers config
-	#define PDS "--SERVER=10.129.28.141:12000 --SERVER=10.129.26.246:12000 --SERVER=10.129.26.81:12000"
-#endif
 using namespace std;
-
-#if DSR != 99 
-using namespace kvstore;   
-#endif
+using namespace kvstore;
 
 
 typedef std::chrono::high_resolution_clock CLOCK;
 typedef std::chrono::microseconds MICROSECONDS;
 
-
-#define MME1 "10.129.26.127"
-#define MME2 "10.129.28.45" 
-#define MME3 "10.129.26.118" 
-
-#define SGW "10.129.28.191"
-#define SGW2 "10.129.28.224" 
-#define SGW3 "10.129.26.140"
-
-#define PGW3 "10.129.28.147"
-#define PGW2 "10.129.26.141" 
-#define PGW1 "10.129.26.148" 
-
-#define MMELB "10.129.26.199"
-#define SGWLB "10.129.26.202"
-#define PGWLB "10.129.26.201"
-
-#define HSS "10.129.28.188"
-
-#define RAN "10.129.26.170"
-#define RAN2 "10.129.26.111"
-#define RAN3 "10.129.26.214"
-
-#define SINK "10.129.28.36"
-#define SINK2 "10.129.26.235"
-#define SINK3 "10.129.26.225"
-
-#define INIT_VAL 100000000   // to set with different init value for other mme replicas e.g. next replica could start with 400000000
-//#define INIT_VAL 400000000   // to set with different init value for other mme replicas e.g. next replica could start with 700000000
-//#define INIT_VAL 700000000   // to set with different init value for other mme replicas 
 #define TRACE(x) if (DEBUGG) { x }
 
 const int MAX_UE_COUNT = 10000;
